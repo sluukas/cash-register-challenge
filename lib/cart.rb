b79384cd90a2c39dbc22aa4fc6874b33eaad2e0a
Sandra <sluukas@gmail.com>
2024-04-12 15:02:33 +0200

    cart to string method

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,3 +1,17 @@
-def add_to_cart(cart, product)
-  cart << product
+class Cart
+
+  def add_to_cart(cart, product)
+    cart << product
+  end
+
+
+  def cart_to_s(cart)
+    formatted_items = cart.uniq.map do |item|
+      quantity = cart.count(item)
+      "#{item}#{quantity > 1 ? " x#{quantity}" : ""}"
+    end
+
+    formatted_items.join(', ')
+  end
+
 end
