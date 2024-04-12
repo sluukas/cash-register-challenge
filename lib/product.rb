f710d8a495e9273af183ef85748a98f61d6f37eb
Sandra <sluukas@gmail.com>
2024-04-12 15:23:53 +0200

    products in store method

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,20 @@
 class Product
-  attr_reader :code, :name, :price, :discount
+  def store_items
+    # Define store items with product codes and prices
+    store = {
+      'GR1' => { name: 'Green Tea', price: 3.11 },
+      'SR1' => { name: 'Strawberries', price: 5.00 },
+      'CF1' => { name: 'Coffee', price: 11.23 }
+    }
 
-  def initialize(code, name, price, discount = nil)
-    @code = code
-    @name = name
-    @price = price
-    @discount = discount
+    return store
   end
+
+#   # Get product information using product code
+# product_code = 'GR1'
+# product_info = store_items[product_code]
+# puts "Product name: #{product_info[:name]}"
+# puts "Product price: #{product_info[:price]}€"
+
+
 end
